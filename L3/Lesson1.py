3e698e5282f280e54acebd3147c96adaac9e228f
unknown <hgodzien@gmail.com>
2021-10-10 20:03:45 +0200

    Second commit

--- L3/Lesson1.py
+++ L3/Lesson1.py
@@ -7,8 +7,72 @@ Lecon N°1
 Bases algorithmiques
 """
 
+# float: 0.1
+# int: 0
+# str: "hello"
+# char: "a"
+# bool: True ou False
+# array: [ "str", 12, 1.0, True ]
+# dict: {}
+
+def sub(a: int, b: int):
+    return int(a) - int(b)
+
+def add(a:int, b:int):
+    return a + b
+
+def sub2(a: int, b: int):
+    r = int(a) - int(b)
+    if r % 2 == 0:
+        return 0
+    else:
+        return r
+
+def add2(a:int, b:int):
+    # TODO : faire la meme chose avec un ternaire
+    return 3 if int(a) + int(b) % 1 == 0 else int(a) + int(b)
+
+"""
+4 | 2
+    _
+4   Q = 2
+_
+R = 0 
+
+4 / 2 = 2 <= Q
+4 % 2 = 0 <= R
+"""
 
 if __name__ == "__main__":
-    nom = input("Veuillez saisir votre nom de famille: ")
-    prenom = input("Veuillez saisir votre prénom: ")
-    print(f"Bonjour {prenom} {nom} !")
+    print('Hello World!')
+    a = 12
+    b = 13
+    # print(f" a = {type(a)}")
+    # print(f" b = {type(b)}")
+
+    print(sub(a, b))
+
+    c = 2
+
+    r = c if c % 2 == 0 else c + 1
+    
+    compteur = [1, 2, 3, 4, 5]
+     
+    print(compteur[1])
+
+    i = 0
+
+    # while condition est vrai:
+    #     code
+
+    while i < 5:
+        print(compteur[i])
+        i += 1
+
+    for j in compteur:
+        print(j)
+    
+    liste_de_lettre = "abcdef"
+    
+    for letter in liste_de_lettre:
+        print(letter)
