a87db96992924f12f5e669f9992b8e51538bf1ff
unknown <hgodzien@gmail.com>
2021-10-10 18:48:25 +0200

    Initial Commit

---
+++ L3/Lesson1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+Lecon N°1
+
+Bases algorithmiques
+"""
+
+
+if __name__ == "__main__":
+    nom = input("Veuillez saisir votre nom de famille: ")
+    prenom = input("Veuillez saisir votre prénom: ")
+    print(f"Bonjour {prenom} {nom} !")
